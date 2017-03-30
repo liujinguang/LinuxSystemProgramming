c19723990c6500171af6855d8ba44aeedb1d7ba1
jinguang.liu <jinguang.liu@qq.com>
2017-03-30 10:36:03 -0400

    update semlib.c

--- LinuxSysProgramming/unpv22e/pxsem/semlib.c
+++ LinuxSysProgramming/unpv22e/pxsem/semlib.c
@@ -10,6 +10,12 @@
 #include <sys/stat.h>
 #include <semaphore.h>
 #include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <time.h>
+
+extern char *optarg;
+extern int opterr;
 
 static const char * sem_name = "csim_semaphore";
 
@@ -56,7 +62,10 @@ int csim_sem_timedwait(int secs) {
 	int res = 0;
 
 	memset(&ts, 0, sizeof(struct timespec));
-	ts.tv_nsec = secs;
+	if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
+		return -1;
+	}
+	ts.tv_sec += secs;
 
 	sem_t * sem_id = csim_sem_open();
 
@@ -93,6 +102,71 @@ int csim_sem_unlink() {
 int
 main(int argc, char **argv)
 {
-	//printf("sem value: %d\n", csim_sem_getvalue());
-	csim_sem_wait();
+	int c, res;
+	struct option opts[] = {
+		{"unlink", required_argument, NULL, 'u'},
+		{"close", required_argument, NULL, 'c'},
+		{"get", required_argument, NULL, 'g'},
+		{"post", required_argument, NULL, 'p'},
+		{"wait", required_argument, NULL, 'w'},
+		{"trywait", required_argument, NULL, 't'},
+		{"timedwait", required_argument, NULL, 'd'},
+		{0, 0, 0, 0}
+	};
+
+	opterr = 0;
+
+	while ((c=getopt_long(argc, argv, "gucpwtd:", opts, NULL)) != -1)
+	{
+		switch (c)
+		{
+		case 'u':
+			csim_sem_unlink();
+			break;
+
+		case 'c':
+			csim_sem_close();
+			break;
+
+		case 'w':
+			csim_sem_wait();
+			break;
+
+		case 'p':
+			csim_sem_post();
+			break;
+
+		case 'g':
+			printf("sem value: %d\n", csim_sem_getvalue());
+			break;
+
+		case 't':
+			res = csim_sem_trywait();
+			printf("sem_trywait(): %d\n", res);
+			break;
+
+		case 'd':
+			printf("%d\n", atoi(optarg));
+			res = csim_sem_timedwait(atoi(optarg));
+			printf("sem_timedwait(): %d\n", res);
+			break;
+
+		case '?':
+			printf("Usage: \n"
+			"-p post: post semaphore\n"
+			"-g get: get value\n"
+			"-u unlink: unlink semaphore\n"
+			"-c close: close semaphore\n"
+			"-w wait: wait semaphore\n"
+			"-t trywait: trywait semaphore\n"
+			"-d timedwait: timedwait semaphore\n"
+			);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	return 0;
 }
